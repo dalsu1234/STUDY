25dfafb20638447a3bbbc9502018673dbf6a6ea7
dalsu1234 <dalsu.mac@gmail.com>
2022-03-20 19:16:19 +0900

    .

---
+++ Algorithm/sort/InsertionSortcpp.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+	const int SIZE{10};
+	int arr[SIZE]{1,5,7,3,2,7,5,4,3,0};
+	int temp{};
+	int j{};
+	for (int i = 1; i < SIZE; i++)
+	{
+		temp = arr[i];
+		j = i;
+		while (j >= 0 && arr[j-1] > arr[j])
+		{
+			arr[j] = arr[j - 1];
+			arr[j - 1] = temp;
+			j--;
+		}
+	}
+	for (int i =0; i<SIZE;i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+}
