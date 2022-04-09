341df4fea85fb180b8e9f9720f9c6d039cb939b9
dalsu1234 <dalsu.mac@gmail.com>
2022-04-10 00:53:02 +0900

    ~String() error

--- ClassPractice/StringClass/StringClass/toString.cpp
+++ ClassPractice/StringClass/StringClass/toString.cpp
@@ -1,27 +1,2 @@
 #include <iostream>
 #include "toString.h"
-
-String::String(const char* input)
-{
-	int index{};
-	bool isExit{ true };
-	while (isExit)
-	{
-		input[index++];
-		if (input[index] == '\0')
-		{
-			isExit = false;
-		}
-	}
-	mLetter = new char[index];
-	for (int i = 0; i <= index; i++)
-	{
-		mLetter[i] = input[i];
-	}
-
-}
-
-void String::Print()
-{
-	std::cout << mLetter << std::endl;
-}
