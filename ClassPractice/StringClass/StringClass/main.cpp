5001ce0920389443d0e1d30a66490777ae1e98ac
dalsu1234 <dalsu.mac@gmail.com>
2022-04-11 20:13:07 +0900

    operator+() error

--- ClassPractice/StringClass/StringClass/main.cpp
+++ ClassPractice/StringClass/StringClass/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include "toString.h"
-
+#include <string.h>
 int main()
 {
-	String string{ "asdasd" };
-	string.Print();
+	String st1("test");
+	String st2("1234");
+	String st3("1234");
+	String st4(20);
+	String st5(st1);
+	String st6;
+	
+	st2 = st1 + st3;
+	std::cout << st2 << std::endl;
+	std::cout << st5 << std::endl;
+	std::cout << st6 << std::endl;
+	std::cout << st4 << std::endl;
+	std::cout << st3 << std::endl;
+
 }
--- ClassPractice/StringClass/StringClass/toString.cpp
+++ ClassPractice/StringClass/StringClass/toString.cpp
@@ -1,2 +1,75 @@
 #include <iostream>
+#include <string.h>
 #include "toString.h"
+
+String::String()
+{
+	mString = new char[1];
+	mString[0] = '\0';
+	mLength = 0;
+}
+
+String::String(const int index)
+{
+	mString = new char[index+1];
+	mString[0] = '\0';
+	mLength = index;
+}
+
+String::String(const char* string) : String(strlen(string))
+{
+	strcpy_s(mString, strlen(string)+1, string);
+}
+
+String::String(const String& instance)
+{
+	mLength = instance.mLength;
+	mString = new char[mLength+1];
+	for (int i = 0; i < mLength; i++)
+	{
+		mString[i] = instance.mString[i];
+	}
+	mString[mLength] = '\0';
+}
+
+char* String::GetString() const
+{
+	return mString;
+}
+
+int String::GetLength() const
+{
+	return mLength;
+}
+
+
+
+// 연산자 오버로딩
+
+
+String String::operator+(const String& instance)
+{
+	int index = mLength + instance.mLength;
+	String sumString(index);
+
+	
+	for (int i = 0; i < mLength; i++)
+	{
+		sumString.mString[i] = mString[i];
+	}
+	for (int i = 0; i < instance.mLength; i++)
+	{
+		sumString.mString[mLength + i] = instance.mString[i];
+	}
+	sumString.mString[index] = '\0';
+	return sumString;
+}
+
+
+
+
+std::ostream& operator<<(std::ostream& os, const String& string)
+{
+	os << string.mString;
+	return os;
+}
